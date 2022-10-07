348da6e0080b130c095b468491dc20ce14a45edc
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-07 19:58:48 +0530

    Update and rename first.cpp to intro.cpp

--- palak/first.cpp
+++
@@ -1,2 +0,0 @@
-Name- Palak Patel
-Branch- Electronic and communication enginering
---
+++ palak/intro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name,branch,city;
+    float cgpa;
+
+    cin>>name>>branch>>city;
+    cin>>cgpa;
+
+    cout<<"My name is"<<" "<<name<<endl;
+    cout<<"My branch is"<<" " << branch<<endl;
+    cout<<"I am from"<<" "<<city<<endl;
+    cout<<"My 1st year CGPA was"<<" "<<cgpa<<endl;
+
+}
