94e17e0c043aa44a1cd6e6fdd5d54dcc5d257551
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-09 01:15:29 +0530

    Create array.cpp

---
+++ palak/array.cpp
@@ -0,0 +1,18 @@
+int main()
+{
+int arr[10];
+    for(int i=0;i<10;i++)
+    {
+          cin>>arr[i];
+    }
+    for(int i=0;i<10;i++)
+  {
+    cout<<arr[i]<<endl;
+    cout<< "size of (arr[1])"<<" ";
+    cout<<"size of (arr)"<<" ";
+    cout<<" size of (arr)/size of (arr[0])"<<endl;
+  }
+  return 0;
+}
+    
+   
