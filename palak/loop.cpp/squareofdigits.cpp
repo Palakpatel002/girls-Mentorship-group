563dc7d0d9bb15bdf03f5ef8ac7804e114cc1696
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-08 15:59:16 +0530

    Create squareofdigits.cpp

---
+++ palak/loop.cpp/squareofdigits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int i;
+  cout<<"Square of numbers from 1 to 10 are:";
+  for(i=1;i<=10;i++)
+  {
+    cout<<"\n square of"<<i<<"is"<<i*i<<endl;
+  }
+  return 0;
+}
+
