4a98b726e12f7cb3b36a85f065e2c7a394870b24
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-08 15:52:03 +0530

    Create countdigits.cpp

---
+++ palak/loop.cpp/countdigits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n,count;
+  cout<<"Write any number:";
+  cin>>n;
+  
+  while(n!=0)
+  {
+    n=n/10;
+    
+  }
+  cout<<"The number of digits in given number is";
+  cout<<count;
+ 
+  return 0;
+}
