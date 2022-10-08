f4b2dc8d132e9bbdefed10f9e66280562e79a6e0
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-08 14:51:54 +0530

    Create if else2.cpp

---
+++ palak/if else.cpp/if else2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int t;
+  //t is define as time
+  if(t<10)
+  {
+    cout<<"Good Morning";
+  }
+  else if(t<20)
+  {
+    cout<<"Good Afternoon";
+  }
+  else
+    cout<<"Good Evening";
+  return 0;
+}
