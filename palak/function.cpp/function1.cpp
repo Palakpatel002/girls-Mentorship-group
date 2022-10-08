da4f60331ca981b9c9e637c843010d5358a4c780
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-09 00:46:52 +0530

    Create function1.cpp

---
+++ palak/function.cpp/function1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void Sum(int a, int b)
+
+{
+    cout<<"a+b"<<endl;
+    return ;
+}
+int suum (int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    suum(2,7);
+    suum(88,32);
+    int a =suum(75,93);
+    int g=suum(543,879);
+    cout<<suum(2,7)<<endl;
+    cout<<suum(88,32)<<endl;
+    cout<<a<<endl;
+    cout<<g<<endl;
+}
