4a92e898cfd26b8cb8a25fc6fb09fe61c58977db
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-09 17:01:21 +0530

    Create sizeofvecyor.cpp

---
+++ palak/vector.cpp/sizeofvecyor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+  vector<int>vec={20,35,78,86,96,100};
+  vec[5]=100;
+  
+  for(int i=0; i<vec.size(); i++){
+    cout<<vec[i]<<" ";
+
+  }
+  
+  cout<<endl<<vec.size();
+  
+  return 0;
+}
