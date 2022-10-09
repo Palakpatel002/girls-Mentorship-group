98f17d2a0e0c62d7460ec31054bc57d03f9f56bc
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-09 17:07:12 +0530

    Create decreasingsortvector.cpp

---
+++ palak/vector.cpp/decreasingsortvector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+  vector<int>vec={57,78,75,234,765,24,578,54};
+  sort(vec.begin(),vec.end(),greater<int>());
+  for(int i=0; i<vec.size(); i++){
+    cout<<vec[i]<<" ";
+
+  }
+  cout<<endl;
+}
