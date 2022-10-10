a88f22ab3ea963e439b87d2cd9a07757555e59d2
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-10 18:44:16 +0530

    Create pointer2.cpp

---
+++ palak/pointer.cpp/pointer2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int arr[]={10,20,30,40,50};
+  cout<<*(arr)<<endl;
+  cout<<*(arr+0)<<endl;
+   cout<<*(arr+1)<<endl;
+    cout<<*(arr+3)<<endl;
+  
+  return 0;
