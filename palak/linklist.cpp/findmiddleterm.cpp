0e4a9e64b76d5f562049754b8d3d7869498ae237
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-10 19:11:25 +0530

    Create findmiddleterm.cpp

---
+++ palak/linklist.cpp/findmiddleterm.cpp
@@ -0,0 +1,16 @@
+class solution{
+    public:
+    ListNode* middleNode( ListNode* head)
+    {
+        if (head==nullptr)
+        return head;
+        ListNode* slow=head,*fast=head;
+        while(fast!=nullptr&&fast->next!=nullptr)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+        
+    }
+};
