f76fc2cfab6c0f9eaac4ba5344438de3888ef370
Palakpatel002 <114545867+Palakpatel002@users.noreply.github.com>
2022-10-10 18:43:41 +0530

    Create pointer1.cpp

---
+++ palak/pointer.cpp/pointer1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a=15;
+  int *p=&a;
+  int **q=*p;
+  cout<<"a"<<a<<endl;
+  cout<<"p"<<p<<endl;
+  cout<<"*p"<<*p<<endl;
+   cout<<"q"<<q<<endl;
+   cout<<"*q"<<*q<<endl;
+  
+  return 0;
+}
